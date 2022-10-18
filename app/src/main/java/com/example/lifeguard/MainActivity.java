c8dd656f87f2a454a484c925bc0c7bf76b290211
mateib47 <m.bucur@student.utwente.nl>
2022-10-18 23:59:39 +0900

    add initial sms reader and basic ui

--- app/src/main/java/com/example/lifeguard/MainActivity.java
+++ app/src/main/java/com/example/lifeguard/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.lifeguard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readSms();
+            }
+        });
+
+    }
+
+    protected void readSms() {
+        int REQUEST_CODE_ASK_PERMISSIONS = 123;
+        //fixme permission does not work
+        ActivityCompat.requestPermissions(this, new String[]{"android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        System.out.println("okkkk");
+        if (cursor.moveToFirst()) {
+            do {
+                String msgData = "";
+                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                }
+                System.out.println(msgData);
+            } while (cursor.moveToNext());
+        } else {
+            System.out.println("No SMS in inbox");
+        }
     }
 }
