36c32000bd6c3366160df8949935654e4072da41
mateib47 <m.bucur@student.utwente.nl>
2022-10-20 23:59:01 +0900

    read sent messages; filter columns and date

--- app/src/main/java/com/example/lifeguard/MainActivity.java
+++ app/src/main/java/com/example/lifeguard/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,17 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
     protected void readSms() {
         int REQUEST_CODE_ASK_PERMISSIONS = 123;
-        //fixme permission does not work
         ActivityCompat.requestPermissions(this, new String[]{"android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
-        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
-        System.out.println("okkkk");
+        //Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        final String[] projection = new String[]{"_id", "address", "body", "date"};
+        final Uri uri = Uri.parse("content://sms/sent");
+        Cursor cursor = getContentResolver().query(uri, projection,null, null, null);
         if (cursor.moveToFirst()) {
             do {
                 String msgData = "";
+                boolean good = false;
                 for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
-                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx) + "\n";
+
+                    if(cursor.getColumnName(idx).equals("date") ){
+                        long date = Long.parseLong( cursor.getString(idx));
+                        //get messages from the last week
+                        if(date > System.currentTimeMillis() - TimeUnit.DAYS.toMillis(5)){
+                            good = true;
+                        }
+                    }
                 }
-                System.out.println(msgData);
+                if(good)
+                    System.out.println("MsgData: " + msgData);
             } while (cursor.moveToNext());
         } else {
             System.out.println("No SMS in inbox");
